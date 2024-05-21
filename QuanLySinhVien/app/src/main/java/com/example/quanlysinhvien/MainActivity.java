c02f95c0063efd73d28f7250493e0721058295d9
nmthanh31 <nmthanh3107.03@gmail.com>
2024-05-21 08:19:36 +0700

    commitLanDauNe

--- QuanLySinhVien/app/src/main/java/com/example/quanlysinhvien/MainActivity.java
+++ QuanLySinhVien/app/src/main/java/com/example/quanlysinhvien/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.quanlysinhvien;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +18,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtMaLop, edtTenLop, edtSiSo;
+
+    private Button btnInsert, btnDelete, btnUpdate, btnQuery;
+
+    private ListView lvSinhVien;
+
+    private ArrayList<String> listSinhVien;
+    private ArrayAdapter<String> sinhVienAdapter;
+
+    private SQLiteDatabase mySql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +43,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtMaLop = findViewById(R.id.edtMaLop);
+        edtTenLop = findViewById(R.id.edtTenLop);
+        edtSiSo = findViewById(R.id.edtSiSo);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnQuery = findViewById(R.id.btnQuery);
+
+        lvSinhVien = findViewById(R.id.lvSinhVien);
+
+        listSinhVien = new ArrayList<>();
+        sinhVienAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listSinhVien);
+
+        lvSinhVien.setAdapter(sinhVienAdapter);
+
+        mySql = openOrCreateDatabase("qlsinhvien.db",MODE_PRIVATE,null);
+
+        try {
+            String sql = "CREATE TABLE tbllop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
+            mySql.execSQL(sql);
+        }catch (Exception e){
+            Log.e("Error","Table ko tồn tại");
+        }
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String malop = edtMaLop.getText().toString();
+                String tenlop = edtTenLop.getText().toString();
+
+                int siso = Integer.parseInt(edtSiSo.getText().toString());
+
+                ContentValues content = new ContentValues();
+                content.put("malop",malop);
+                content.put("tenlop",tenlop);
+                content.put("siso",siso);
+
+                if (mySql.insert("tbllop",null,content) == -1){
+                    Toast.makeText(MainActivity.this, "Thêm thất bại!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Thêm thành công!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
